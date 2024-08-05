4466b0ee9bbe13ce800f4f167ac527bf971e70c0
riskkif <riskisam78@gmail.com>
2024-08-05 09:42:14 +0700

    adding autorun and dockerfile

--- autorun.sh
+++ autorun.sh
@@ -5,7 +5,7 @@
 docker build -t admintsxproject:1.0 .
 
 # Remove any existing container named 
-docker rm -f admintsxproject || true
+docker rm  admintsxproject || true
 
 # Run the Docker container with specified options
 docker run --name=admintsxproject --hostname=admintsxproject --volume=/root/admintsxproject:/admintsxproject --workdir=/admintsxproject -p 5137:5137 --restart=no --runtime=runc admintsxproject:1.0 npm run dev --host=0.0.0.0 --reload
